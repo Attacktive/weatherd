e0e256242ea01f7c5854c020038262f878655d0a
Attacktive <tribute2pantera@gmail.com>
2026-09-19 13:28:33 +0900

    chore: bump version to 3.1.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
 		applicationId = "xyz.attacktive.weatherd"
 		minSdk = 26
 		targetSdk = 37
-		versionCode = 28
-		versionName = "3.1.4"
+		versionCode = 29
+		versionName = "3.1.5"
 		testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 	}
 
